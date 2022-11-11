f2165ef05e59f31a5347ec1727b82e895eb8a791
William Civa <williamciva@outlook.com>
2022-11-11 20:56:01 -0300

    Create template spring boot

---
+++ src/main/java/br/com/rest/inventorycontrol/inventorycontrol/InventorycontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest.inventorycontrol.inventorycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorycontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorycontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rest/inventorycontrol/inventorycontrol/InventorycontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.inventorycontrol.inventorycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorycontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
