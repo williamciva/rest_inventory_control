abdadab12c21dfcb0c1ebcdd887e9182f518d1d3
William Civa <williamciva@outlook.com>
2022-11-11 21:32:39 -0300

    Create intials class

---
+++ src/main/java/br/com/rest/inventorycontrol/inventorycontrol/Inventory.java
@@ -0,0 +1,87 @@
+package br.com.rest.inventorycontrol.inventorycontrol;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Objects;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Inventory {
+    
+	private @Id @GeneratedValue BigInteger codProd;
+	private String nameProd;
+	private BigInteger codBar;
+    private BigDecimal quantity;
+    private BigDecimal price;
+
+	Inventory() {}
+
+	Inventory(String nameProd, BigInteger codBar, BigDecimal quantity, BigDecimal price) {
+		this.nameProd = nameProd;
+        this.codBar = codBar;
+        this.quantity = quantity;
+        this.price = price;
+	}
+
+	public BigInteger getCodProd() {
+        return codProd;
+    }
+
+    public String getNameProd() {
+        return nameProd;
+    }
+
+    public BigInteger getCodBar() {
+        return codBar;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setNameProd(String nameProd) {
+        this.nameProd = nameProd;
+    }
+
+    public void setCodBar(BigInteger codBar) {
+        this.codBar = codBar;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+	public boolean equals(Object o) {
+
+		if (this == o)
+			return true;
+		if (!(o instanceof Inventory))
+			return false;
+            Inventory inventory = (Inventory) o;
+		return Objects.equals(this.codProd, inventory.codProd) && Objects.equals(this.nameProd, inventory.nameProd)
+				&& Objects.equals(this.codBar, inventory.codBar) && Objects.equals(this.quantity, inventory.quantity)
+                && Objects.equals(this.price, inventory.price);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.codProd, this.nameProd, this.codBar, this.quantity, this.price);
+	}
+
+	@Override
+	public String toString() {
+		return "Inventory{" + "codProd=" + this.codProd + ", nameProd='" + this.nameProd + ", codBar='" + this.codBar + 
+        ", quantity='" + this.quantity + ", price='" + this.price + '\'' + '}';
+	}
+    
+}
