bcbde86950178747f8740aac3724b20151376149
williamciva <william.civa@outlook.com>
2022-11-14 17:16:21 -0300

    Swagger documentation improvements and auto increment for ID ( codProd )

--- src/main/java/br/com/rest/inventorycontrol/inventorycontrol/Inventory.java
+++ src/main/java/br/com/rest/inventorycontrol/inventorycontrol/Inventory.java
@@ -1,107 +1,168 @@
 package br.com.rest.inventorycontrol.inventorycontrol;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import io.swagger.annotations.ApiModelProperty;
 
 @Entity
 public class Inventory {
-    
-    @ApiModelProperty(value = "Código do Produto")
-    @Id 
-    @GeneratedValue
-	private Long codProd;
-    
-    @ApiModelProperty(value = "Nome do Produto")
-	private String nameProd;
 
-    @ApiModelProperty(value = "Código de barras")
-	private BigInteger codBar;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @ApiModelProperty(name = "Código do Produto", example = "1", position = 0, readOnly = true)
+  private Long codProd;
 
-    @ApiModelProperty(value = "Quantidade em Estoque")
-    private BigDecimal quantity;
+  @ApiModelProperty(
+    name = "Nome do Produto",
+    example = "Blue Pen",
+    position = 1
+  )
+  private String nameProd;
 
-    @ApiModelProperty(value = "Preço de Venda")
-    private BigDecimal price;
 
-    
-	Inventory() {}
 
-	Inventory(String nameProd, BigInteger codBar, BigDecimal quantity, BigDecimal price) {
-		this.nameProd = nameProd;
-        this.codBar = codBar;
-        this.quantity = quantity;
-        this.price = price;
-	}
+  @ApiModelProperty(
+    name = "Código de barras",
+    example = "789765235113",
+    position = 2
+  )
+  private BigInteger codBar;
 
-	public Long getCodProd() {
-        return codProd;
-    }
 
-    public String getNameProd() {
-        return nameProd;
-    }
 
-    public BigInteger getCodBar() {
-        return codBar;
-    }
+  @ApiModelProperty(
+    name = "Quantidade em Estoque",
+    example = "136",
+    position = 3
+  )
+  private BigDecimal quantity;
 
-    public BigDecimal getQuantity() {
-        return quantity;
-    }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
 
-    public void setNameProd(String nameProd) {
-        this.nameProd = nameProd;
-    }
+  @ApiModelProperty(name = "Preço de Venda", example = "1.50", position = 4)
+  private BigDecimal price;
 
-    public void setCodBar(BigInteger codBar) {
-        this.codBar = codBar;
-    }
 
-    public void setQuantity(BigDecimal quantity) {
-        this.quantity = quantity;
-    }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+  Inventory() {}
 
-    public void setCodProd(Long codProd) {
-        this.codProd = codProd;
-    }
+  Inventory(
+    String nameProd,
+    BigInteger codBar,
+    BigDecimal quantity,
+    BigDecimal price
+  ) {
+    this.nameProd = nameProd;
+    this.codBar = codBar;
+    this.quantity = quantity;
+    this.price = price;
+  }
 
-    @Override
-	public boolean equals(Object o) {
 
-		if (this == o)
-			return true;
-		if (!(o instanceof Inventory))
-			return false;
-            Inventory inventory = (Inventory) o;
-		return Objects.equals(this.codProd, inventory.codProd) && Objects.equals(this.nameProd, inventory.nameProd)
-				&& Objects.equals(this.codBar, inventory.codBar) && Objects.equals(this.quantity, inventory.quantity)
-                && Objects.equals(this.price, inventory.price);
-	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.codProd, this.nameProd, this.codBar, this.quantity, this.price);
-	}
+  public Long getCodProd() {
+    return codProd;
+  }
 
-	@Override
-	public String toString() {
-		return "Inventory{" + "codProd=" + this.codProd + ", nameProd='" + this.nameProd + ", codBar='" + this.codBar + 
-        ", quantity='" + this.quantity + ", price='" + this.price + '\'' + '}';
-	}
-    
+
+  public String getNameProd() {
+    return nameProd;
+  }
+
+
+  public BigInteger getCodBar() {
+    return codBar;
+  }
+
+
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
+
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+
+  public void setNameProd(String nameProd) {
+    this.nameProd = nameProd;
+  }
+
+
+  public void setCodBar(BigInteger codBar) {
+    this.codBar = codBar;
+  }
+
+
+  public void setQuantity(BigDecimal quantity) {
+    this.quantity = quantity;
+  }
+
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+
+  public void setCodProd(Long codProd) {
+    this.codProd = codProd;
+  }
+
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Inventory)) return false;
+    Inventory inventory = (Inventory) o;
+    return (
+      Objects.equals(this.codProd, inventory.codProd) &&
+      Objects.equals(this.nameProd, inventory.nameProd) &&
+      Objects.equals(this.codBar, inventory.codBar) &&
+      Objects.equals(this.quantity, inventory.quantity) &&
+      Objects.equals(this.price, inventory.price)
+    );
+  }
+
+
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+      this.codProd,
+      this.nameProd,
+      this.codBar,
+      this.quantity,
+      this.price
+    );
+  }
+
+
+
+  @Override
+  public String toString() {
+    return (
+      "Inventory{" +
+      "codProd=" +
+      this.codProd +
+      ", nameProd='" +
+      this.nameProd +
+      ", codBar='" +
+      this.codBar +
+      ", quantity='" +
+      this.quantity +
+      ", price='" +
+      this.price +
+      '\'' +
+      '}'
+    );
+  }
 }
--- src/main/java/br/com/rest/inventorycontrol/inventorycontrol/InventoryController.java
+++ src/main/java/br/com/rest/inventorycontrol/inventorycontrol/InventoryController.java
@@ -1,5 +1,7 @@
 package br.com.rest.inventorycontrol.inventorycontrol;
 
+import io.swagger.annotations.ApiOperation;
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,33 +21,56 @@ class InventoryController {
     this.repository = repository;
   }
 
-
   // Aggregate root
   // tag::get-aggregate-root[]
+  @ApiOperation(
+    value = "Retorna uma lista lista de todos os produtos.",
+    position = 0
+  )
   @GetMapping("/products")
   List<Inventory> all() {
     return repository.findAll();
   }
   // end::get-aggregate-root[]
 
+
+
+  // Single item
+  @ApiOperation(
+    value = "Retorna um JSON do produto informado.",
+    position = 1
+  )
+  @GetMapping("/products/{codProd}")
+  Inventory one(@PathVariable Long codProd) {
+    return repository
+      .findById(codProd)
+      .orElseThrow(() -> new InvetoryNotFoundException(codProd));
+  }
+
+
+
+  @ApiOperation(
+    value = "Insere um novo produto.",
+    position = 2
+  )
   @PostMapping("/products")
   Inventory newInventory(@RequestBody Inventory newInventory) {
     return repository.save(newInventory);
   }
 
-  // Single item
-  
-  @GetMapping("/products/{codProd}")
-  Inventory one(@PathVariable Long codProd) {
-    
-    return repository.findById(codProd)
-      .orElseThrow(() -> new InvetoryNotFoundException(codProd));
-  }
 
+
+  @ApiOperation(
+    value = "Edita produtos.",
+    position = 3
+  )
   @PutMapping("/products/{codProd}")
-  Inventory replaceEmployee(@RequestBody Inventory newInventory, @PathVariable Long codProd) {
-    
-    return repository.findById(codProd)
+  Inventory replaceEmployee(
+    @RequestBody Inventory newInventory,
+    @PathVariable Long codProd
+  ) {
+    return repository
+      .findById(codProd)
       .map(inventory -> {
         inventory.setNameProd(newInventory.getNameProd());
         return repository.save(inventory);
@@ -56,6 +81,12 @@ class InventoryController {
       });
   }
 
+
+
+  @ApiOperation(
+    value = "Deleta um produto.",
+    position = 4
+  )
   @DeleteMapping("/products/{codProd}")
   void deleteEmployee(@PathVariable Long codProd) {
     repository.deleteById(codProd);
